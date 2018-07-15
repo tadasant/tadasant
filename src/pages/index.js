7de1eb02792301eeb342d7c02bdfa4eb89515949
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2018-07-14 21:37:07 -0400

    add linkedin

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,25 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import gitHubLogo from '../../assets/github.svg';
+import linkedInLogo from '../../assets/linkedin.svg';
 
 const IndexPage = () => (
   <Layout>
     <h1 style={{textAlign: 'center'}}>🚧 Under Construction! 🚧</h1>
-      <a href='https://github.com/tadasant/tadasant'>
-        <img style={{maxHeight: '2em'}} src={gitHubLogo} alt='GitHub Logo' />
-      </a>
+
+    <div style={{display: 'flex', justifyContent: 'center'}}>
+      <div style={{paddingRight: '2vw'}}>
+        <a href='https://github.com/tadasant/tadasant'>
+          <img style={{height: '2em'}} src={gitHubLogo} alt='GitHub Logo'/>
+        </a>
+      </div>
+      <div style={{paddingLeft: '2vw'}}>
+        <a href='https://www.linkedin.com/in/antanavicius/'>
+          <img style={{height: '2em'}} src={linkedInLogo} alt='LinkedInLogo'/>
+        </a>
+      </div>
+    </div>
+
   </Layout>
 )
 
