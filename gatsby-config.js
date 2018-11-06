2fe3efbb445a6c161b230d4f4dff45e409ce4c17
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2018-11-06 07:02:43 -0800

    added filesystem plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,13 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
+    'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`,
+      },
+    },
   ],
 }
