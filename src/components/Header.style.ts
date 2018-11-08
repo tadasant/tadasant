48ad8be44f02afdcc0c6f5012fe84ab7c8d56cd2
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2018-11-08 07:42:53 -0800

    header layout

--- src/components/Header.style.ts
+++ src/components/Header.style.ts
@@ -2,12 +2,39 @@ import styled from 'styled-components'
 import constants from '../styling/constants'
 import { media } from '../styling/core'
 
-export const Container = styled.div`
-  height: 48px;
+export const ContainerDiv = styled.div`
   background-color: ${constants.colors.brand};
   box-shadow: 0px 2px 4px 0 ${constants.colors.shadow};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 32px;
+  
+  padding: 8px;
   
   ${media.tablet`
-    height: 64px;
+    padding: 16px;
+  `}
+`;
+
+export const MobileNavigationContainerDiv = styled.div`
+  ${media.tablet`
+    display: none;
+  `}
+`;
+
+export const TabletNavigationContainerDiv = styled.div`
+  display: none;
+  
+  ${media.tablet`
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  `}
+`;
+
+export const MobilePageHeaderContainerDiv = styled.div`
+  ${media.tablet`
+    display: none;
   `}
 `
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,36 @@
 import { SFC } from 'react'
 import * as React from 'react';
-import { Container } from './Header.style'
+import {
+  ContainerDiv,
+  TabletNavigationContainerDiv,
+  MobilePageHeaderContainerDiv,
+  MobileNavigationContainerDiv,
+} from './Header.style'
 
 const Header: SFC = () => {
   return (
-    <Container>
-      Stuff
-    </Container>
+    <ContainerDiv>
+      <div>
+        Logo
+      </div>
+      <MobilePageHeaderContainerDiv>
+        Page name
+      </MobilePageHeaderContainerDiv>
+      <MobileNavigationContainerDiv>
+        Menu bar
+      </MobileNavigationContainerDiv>
+      <TabletNavigationContainerDiv>
+        <div>
+          Nav1
+        </div>
+        <div>
+          Nav2
+        </div>
+        <div>
+          Nav3
+        </div>
+      </TabletNavigationContainerDiv>
+    </ContainerDiv>
   )
 }
 
