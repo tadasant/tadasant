cd1bbb827ffb1971e1ba317f0332f773faf88fff
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2018-11-09 11:50:00 -0800

    footer without text

---
+++ src/components/footer/Footer.style.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const FooterContainerDiv = styled.div`
+  display: flex;
+  align-items: center;
+  
+  text-align: center;
+`;
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import { SFC } from 'react';
+import * as React from 'react';
+
+const Footer: SFC = () => {
+  return (
+    <div />
+  );
+};
+
+export default Footer;
