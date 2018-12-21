e2ebe7d6b100ef3c7af8e2b0d9a6e9a8cad4eadd
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2018-12-21 14:45:48 -0800

    added links to tablet

--- src/components/home/children/Tablet.tsx
+++ src/components/home/children/Tablet.tsx
@@ -1,20 +1,37 @@
 import * as React from 'react';
 import { FunctionComponent } from 'react';
+import { UndecoratedAnchor } from '../../lib/styled-lib';
 import { Sup, TabletContentDiv, TabletDiv, TabletText } from './Tablet.style';
 
 const Tablet: FunctionComponent = () => {
   return (
     <TabletDiv>
       <TabletContentDiv>
-        <TabletText white>I can always do better <Sup>1</Sup></TabletText>
+        <UndecoratedAnchor href='https://www.goodreads.com/review/show/2227051553' rel='noopener nofollow'
+                           target='_blank'>
+          <TabletText white>I can always do better <Sup>1</Sup></TabletText>
+        </UndecoratedAnchor>
         <br/><br/>
-        <TabletText white>Give without expectation of return <Sup>2</Sup></TabletText>
+        <UndecoratedAnchor href='https://www.goodreads.com/review/show/2227033457' rel='noopener nofollow'
+                           target='_blank'>
+          <TabletText white>Give without expectation of return <Sup>2</Sup></TabletText>
+        </UndecoratedAnchor>
         <br/><br/>
-        <TabletText white>None of us really has it figured out <Sup>3</Sup></TabletText>
+        <UndecoratedAnchor href='https://www.goodreads.com/review/show/2273757184' rel='noopener nofollow'
+                           target='_blank'>
+          <TabletText white>None of us really has it figured out <Sup>3</Sup></TabletText>
+        </UndecoratedAnchor>
         <br/><br/>
-        <TabletText white>Incentives are a super power <Sup>4</Sup></TabletText>
+        <UndecoratedAnchor
+          href='https://quotefancy.com/quote/1561882/Charlie-Munger-Show-me-the-incentive-and-I-will-show-you-the-outcome'
+          rel='noopener nofollow' target='_blank'>
+          <TabletText white>Incentives are a super power <Sup>4</Sup></TabletText>
+        </UndecoratedAnchor>
         <br/><br/>
-        <TabletText white>It's hard to do good by doing well <Sup>5</Sup></TabletText>
+        <UndecoratedAnchor href='https://www.goodreads.com/review/show/2603781234' rel='noopener nofollow'
+                           target='_blank'>
+          <TabletText white>It's hard to do good by doing well <Sup>5</Sup></TabletText>
+        </UndecoratedAnchor>
       </TabletContentDiv>
     </TabletDiv>
   );
