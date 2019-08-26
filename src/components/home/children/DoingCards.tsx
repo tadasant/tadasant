946625018e0f06ec706b2dc06078b0cbdb0ffec0
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2019-08-26 06:27:28 -0700

    update doing cards

--- src/components/home/children/DoingCards.tsx
+++ src/components/home/children/DoingCards.tsx
@@ -1,7 +1,7 @@
-import * as React from 'react';
-import { FunctionComponent } from 'react';
-import DoingCard, { ICardProps } from './DoingCard';
-import {DoingCardsContainerDiv} from './DoingCards.style';
+import * as React from 'react'
+import { FunctionComponent } from 'react'
+import DoingCard, { ICardProps } from './DoingCard'
+import { DoingCardsContainerDiv } from './DoingCards.style'
 
 const cardProps: Partial<ICardProps>[] = [
   {
@@ -10,58 +10,72 @@ const cardProps: Partial<ICardProps>[] = [
   },
   {
     title: 'Connecting',
-    description: 'Having just landed in SF, my focus is to meet interesting people in tech and entreprenurship. Let\'s connect!',
+    description:
+      "Having just landed in SF, my focus is to meet interesting people in tech and entreprenurship. Let's connect!",
     urlDisplay: 'tadas@tadasant.com',
     url: 'mailto:tadas@tadasant.com?subject=Lets%20Connect',
   },
   {
-    title: 'Blogging',
-    description: 'I write about software, business, and life. Not (yet) on a regular cadence, but publish about once per month.',
-    urlDisplay: 'tadasant.com/blog',
-    url: 'https://tadasant.com/blog',
-  },
-  {
     title: 'BariBuilder',
-    description: 'E-commerce web application built for weight loss surgery patients to help them comparison shop for vitamins.',
+    description:
+      'Educational content site and companion software suite built for weight loss surgery patients.',
     urlDisplay: 'baribuilder.com',
     url: 'https://baribuilder.com/',
   },
   {
+    title: 'Blogging',
+    description:
+      'I write about software, business, and life. Not (yet) on a regular cadence, but publish a few times per year',
+    urlDisplay: 'tadasant.com/blog',
+    url: 'https://tadasant.com/blog',
+  },
+  {
+    title: 'Speaking',
+    description:
+      'I enjoy giving the occasional talk or presentation in line with the kinds of things I would blog about',
+    urlDisplay: 'bit.ly/youtube-tadasant',
+    url: 'http://bit.ly/youtube-tadasant',
+  },
+  {
     title: 'Reading',
-    description: 'Books about society, personal development, economics, psychology, business, and technology.',
+    description:
+      'Books about society, personal development, economics, psychology, business, and technology.',
     urlDisplay: 'tadasant.com/goodreads',
     url: 'https://tadasant.com/goodreads',
   },
   {
     title: 'Listening',
-    description: 'Podcasts about startups, software, and psychology. Always eager to discuss a thought-provoking episode.',
+    description:
+      'Podcasts about startups, software, and psychology. Always eager to discuss a thought-provoking episode.',
   },
   {
-    title: 'StackOverflow',
-    description: 'Spend a few hours now and then answering ReactJS questions to keep a finger on the community pulse.',
-    urlDisplay: 'tadasant.com/stackoverflow',
-    url: 'https://tadasant.com/stackoverflow',
+    title: 'Ships',
+    description:
+      "Hobby, non-commercializable side project I'm toying with with a focus on managing and fostering relationships: friendships, professional connections, etc.",
+    urlDisplay: 'github.com/tadasant/ships',
+    url: 'https://github.com/tadasant/ships',
   },
-  {
-    title: 'Where In The World',
-    description: 'Hackathon project that\'s become a small side project. No grand ambitions (for now) besides being a technical playground.',
-    urlDisplay: 'tadasant.com/where-in-the-world',
-    url: 'https://tadasant.com/where-in-the-world',
-  },
-];
+]
 
 const DoingCards: FunctionComponent = () => {
   return (
     <DoingCardsContainerDiv>
       {cardProps.map((props, i) => {
         if (!props.title || !props.description) {
-          return null;
+          return null
         }
-        // @ts-ignore doesn't recognize null check above
-        return <DoingCard key={i} {...props} totalCardCount={cardProps.length} cardIndex={i}/>;
+        return (
+          // @ts-ignore doesn't recognize null check above
+          <DoingCard
+            key={i}
+            {...props}
+            totalCardCount={cardProps.length}
+            cardIndex={i}
+          />
+        )
       })}
     </DoingCardsContainerDiv>
-  );
-};
+  )
+}
 
-export default DoingCards;
+export default DoingCards
